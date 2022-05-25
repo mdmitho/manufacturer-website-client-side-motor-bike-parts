1d9a8e1b332ec8902b415fdd0e22538635e0a8ba
Hossen <mdmitho199911@gmail.com>
2022-05-26 02:38:41 +0600

    adminRow and makeAdmin done

--- src/components/Pages/Dashboard/MakeAdmin/MakeAdmin.js
+++ src/components/Pages/Dashboard/MakeAdmin/MakeAdmin.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../../../Hooks/Loading';
+import AdminRow from '../AdminRow/AdminRow';
 
 const MakeAdmin = () => {
+
+    const {
+      data: users,
+      isLoading,
+      refetch,
+    } = useQuery("users", () =>
+      fetch("http://localhost:5000/user", {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }).then((res) => res.json())
+    );
+    if (isLoading) {
+      return <Loading></Loading>;
+    }
+
+
     return (
-        <div>
-            <h1>make admin</h1>
+      <div>
+        <h2 className="text-2xl">All Users: {users.length}</h2>
+        <div class="overflow-x-auto">
+          <table class="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <AdminRow key={user._id} user={user} refetch={refetch}></AdminRow>
+              ))}
+            </tbody>
+          </table>
         </div>
+      </div>
     );
 };
 
