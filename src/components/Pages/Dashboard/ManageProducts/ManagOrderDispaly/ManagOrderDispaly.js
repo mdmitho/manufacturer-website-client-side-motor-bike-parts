0e70320447e9156d49fd68d4aca7617885f0b295
Hossen <mdmitho199911@gmail.com>
2022-05-26 03:58:23 +0600

    delete parts done

--- src/components/Pages/Dashboard/ManageProducts/ManagOrderDispaly/ManagOrderDispaly.js
+++ src/components/Pages/Dashboard/ManageProducts/ManagOrderDispaly/ManagOrderDispaly.js
@@ -1,28 +1,28 @@
 import React from 'react';
 
-const ManagOrderDispaly = ({part,index}) => {
-    const { name, img, price, description, minimumqQantity, availableQquantity, _id } = part;
-    return (
+const ManagOrderDispaly = ({ part, DeleteBtn }) => {
+  const { name, img, price, description, minimumqQantity, availableQquantity, _id } = part;
+  return (
+    <div>
       <div>
-        <div>
-          <div class=" card card-compact  bg-base-100 shadow-xl">
-            <figure>
-              <img className="h-80" src={img} alt="Shoes" />
-            </figure>
-            <div class="card-body">
-              <h2 class="card-title">Name : {name}</h2>
-              <p>{description}</p>
-              <p>Price : $ {price}</p>
-              <div class="card-actions justify-center">
-                <button class="btn btn-primary text-white">
-                  Buy Now
-                </button>
-              </div>
+        <div class=" card card-compact  bg-base-100 shadow-xl">
+          <figure>
+            <img className="h-80" src={img} alt="Shoes" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Name : {name}</h2>
+            <p>{description}</p>
+            <p>Price : $ {price}</p>
+            <div class="card-actions justify-center">
+              <button onClick={() => DeleteBtn(part._id)} class="btn btn-primary text-white">
+               Delete
+              </button>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default ManagOrderDispaly;
--- src/components/Pages/Dashboard/ManageProducts/ManageProducts.js
+++ src/components/Pages/Dashboard/ManageProducts/ManageProducts.js
@@ -1,33 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../../../../Hooks/Loading';
 import ManagOrderDispaly from './ManagOrderDispaly/ManagOrderDispaly';
 
 const ManageProducts = () => {
-    const {
-        data: parts,
-        isLoading,
-        refetch,
-      } = useQuery(["parts"], () => fetch("http://localhost:5000/parts").then(
-        (res) => res.json()))
+const [parts,setParts ] = useState([])
+
+useEffect(() => {
+  fetch("http://localhost:5000/parts")
+    .then((res) => res.json())
+    .then((data) => setParts(data));
+}, []);
+
+
+
+
+const DeleteBtn = (id) => {
+  const proceed = window.confirm("Are you sure you want to delete");
+  if (proceed) {
+    const url = `http://localhost:5000/parts/${id}`;
+    fetch(url, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("sucess", data);
+        const remaing = parts.filter((item) => item._id !== id);
+        setParts(remaing);
+      });
+  }
+};
+
+
+
+    // const {
+    //     data: parts,
+    //     isLoading,
+    //     refetch,
+    //   } = useQuery(["parts"], () => fetch("http://localhost:5000/parts").then(
+    //     (res) => res.json()))
       
-      if(isLoading){
-        return <Loading />;
-      }
+    //   if(isLoading){
+    //     return <Loading />;
+    //   }
+
+
+
+
+
     return (
-       
-      <div className='container mx-auto'>
-      <h1 className="text-2xl font-bold text-center my-16  text-primary">Bike parts</h1>
-      <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 m-5 justify-center">
-              {parts.map((part, index) => (
-                <ManagOrderDispaly
-                  index={index}
-                  key={part._id}
-                  part={part}
-                  refetch={refetch}
-                > </ManagOrderDispaly>
-              ))}
-              </div>
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold text-center my-16  text-primary">Bike parts</h1>
+        <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 m-5 justify-center">
+          {parts.map((part, index) => (
+            <ManagOrderDispaly
+              index={index}
+              key={part._id}
+              part={part}
+              // refetch={refetch}
+              DeleteBtn={DeleteBtn}
+            >
+              {" "}
+            </ManagOrderDispaly>
+          ))}
+        </div>
       </div>
     );
 };
