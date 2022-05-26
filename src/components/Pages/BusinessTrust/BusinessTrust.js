50b5e580caf2561c96734ff58ae0af8962572902
Hossen <mdmitho199911@gmail.com>
2022-05-26 22:40:26 +0600

    sum chance

--- src/components/Pages/BusinessTrust/BusinessTrust.js
+++ src/components/Pages/BusinessTrust/BusinessTrust.js
@@ -6,7 +6,7 @@ import React from "react";
 const BusinessTrust = () => {
   return (
     <div className="text-center py-16 bg-accent my-5">
-      <section className="container mx-auto py-5 ">
+      <section>
         <h1 className="text-center text-5xl font-bold mt-3 text-primary ">
           {" "}
           MILLIONS PEOPLE TRUST US
@@ -14,14 +14,11 @@ const BusinessTrust = () => {
         <p className="text-center text-white text-2xl font-bold my-5 mb-5">
           TRY TO UNDERSTAND USERS EXPECTATION
         </p>
-
-        <div className="stats shadow mb-16 ,">
-          <div className="stat">
-            <div className="stat-figure text-secondary"></div>
+        <div class="stats stats-vertical lg:stats-horizontal shadow">
+          <div class="stat">
             <div className="stat-title">
               <FontAwesomeIcon icon={faFlag} className="h-11 text-primary"></FontAwesomeIcon>
             </div>
-
             <div className="stat-value">100+</div>
             <div className="stat-desc"> we served Countries</div>
           </div>
@@ -58,7 +55,7 @@ const BusinessTrust = () => {
               ></FontAwesomeIcon>
             </div>
             <div className="stat-value">50+</div>
-            <div className="stat-desc">Best Service</div>
+            <div className="stat-desc">Best Service Review</div>
           </div>
         </div>
       </section>
