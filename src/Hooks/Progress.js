5616816c52e24ade45fdb2deb945753c1fde2768
Hossen <mdmitho199911@gmail.com>
2022-05-26 20:58:44 +0600

    my Skills add

---
+++ src/Hooks/Progress.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const Progress = () => {
+    return (
+      <div
+        className="grid
+      grid-rows-1 gap-5 my-10 "
+      >
+        <h1 className="font-bold text-primary">My Skills</h1>
+        <label htmlFor="">
+          <h1 className="text-success">HTML</h1>
+          <progress class="progress progress-info h-5 w-96" value="100" max="100"></progress>
+        </label>
+        <label htmlFor="">
+          <h1 className="text-success">CSS</h1>
+          <progress class="progress progress-info h-5 w-96" value="100" max="100"></progress>
+        </label>
+        <label htmlFor="">
+          <h1 className="text-success">Bootstarap</h1>
+          <progress class="progress progress-info h-5 w-96" value="100" max="100"></progress>
+        </label>
+
+        <label htmlFor="">
+          <h1 className="text-success">Tailwind CSS</h1>
+          <progress class="progress progress-info h-5 w-96" value="100" max="100"></progress>
+        </label>
+        <label htmlFor="">
+          <h1 className="text-success">JavaScript</h1>
+          <progress class="progress progress-info h-5 w-96" value="95" max="100"></progress>
+        </label>
+        <label htmlFor="">
+          <h1 className="text-success">Git Hub</h1>
+          <progress class="progress progress-info h-5 w-96" value="95" max="100"></progress>
+        </label>
+        <label htmlFor="">
+          <h1 className="text-success">React</h1>
+          <progress class="progress progress-info h-5 w-96" value="95" max="100"></progress>
+        </label>
+        <label htmlFor="">
+          <h1 className="text-success">Firebase</h1>
+          <progress class="progress progress-info h-5 w-96" value="90" max="100"></progress>
+        </label>
+        <label htmlFor="">
+          <h1 className="text-success">MongoDB</h1>
+          <progress class="progress progress-info h-5 w-96" value="90" max="100"></progress>
+        </label>
+
+
+
+
+
+
+
+      </div>
+    );
+};
+
+export default Progress;
--- src/components/Pages/MyPortfolio/MyPortfolio.js
+++ src/components/Pages/MyPortfolio/MyPortfolio.js
@@ -1,12 +1,25 @@
 import React from 'react';
-
+import myimg from '../../../assets/myimg.png'
+import Progress from '../../../Hooks/Progress';
 const MyPortfolio = () => {
     return (
-        <div>
-            <h1>fortfolio</h1>
-            <h1>fortfolio</h1>
-            <h1>fortfolio</h1>
+      <div className="container mx-auto">
+        <div class="card lg:card-side bg-base-100 shadow-xl ">
+          <figure>
+            <img className="" src={myimg} alt="Album" />
+          </figure>
+          <div class="card-body mt-16 ">
+            <h2 class=" card-title ">My Name : Hossen</h2>
+            <h1>Educational : BS.c first years </h1>
+            <h1>I currently want to learn web development at Programming Hero</h1>
+            <p>Email : mdmitho199911@gmail.com</p>
+          </div>
         </div>
+
+    <div className="">
+        <Progress></Progress>
+    </div>
+      </div>
     );
 };
 
